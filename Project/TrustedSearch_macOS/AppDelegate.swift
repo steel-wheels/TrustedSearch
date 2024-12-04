afc5590faef6acd1b49c062ca94f204a69621434
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2024-12-04 21:34:55 +0900

    TSPreferenceController: Initial commit

--- Project/TrustedSearch_macOS/AppDelegate.swift
+++ Project/TrustedSearch_macOS/AppDelegate.swift
@@ -5,6 +5,7 @@
 //  Created by Tomoo Hamada on 2024/11/06.
 //
 
+import MultiUIKit
 import Cocoa
 
 @main
@@ -20,6 +21,25 @@ class AppDelegate: NSObject, NSApplicationDelegate
 
         func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
                 return true
+        }
+
+        private var mDidPreferenceWindowOpended  = false
+        private var mPreferenceWindow: MIWindow? = nil
+
+        @IBAction func openPreferenceWindow(_ sender: NSMenuItem) {
+                if mDidPreferenceWindowOpended {
+                        return
+                }
+
+                let controller = PreferenceViewController()
+                let config = MIWindow.WindowConfig(size: NSSize(width: 640, height: 480), title: "Preference", closeable: true, resizable: false)
+                let window = MIWindow.open(viewController: controller, condfig: config)
+                window.setCallback(windowWillClose: {
+                        () -> Void in
+                        self.mDidPreferenceWindowOpended = false
+                })
+                mPreferenceWindow = window
+                mDidPreferenceWindowOpended = true
         }
 }
 
---
+++ Source/TSPreferenceController.swift
@@ -0,0 +1,50 @@
+/*
+ * @file TSPreferenceController.swift
+ * @description Define TSPreferenceController class
+ * @par Copyright
+ *   Copyright (C) 2024 Steel Wheels Project
+ */
+
+import MultiUIKit
+import Foundation
+
+public class PreferenceViewController: MIViewController
+{
+        public override func viewDidLoad() {
+                super.viewDidLoad()
+
+                let root = MIStack()
+                root.axis = .vertical
+
+                /* remove cache button */
+                root.addArrangedSubView(allocateRemovePreferenceButton())
+
+                self.view = root
+        }
+
+        private func removeCacheFile() {
+                NSLog("Remove cache file")
+        }
+
+        private func allocateRemovePreferenceButton() -> MIStack {
+                let button = MIButton()
+                button.title = "Remove Cache file"
+                button.setCallback({
+                        () -> Void in self.removeCacheFile()
+                })
+                return makeLabeledStack(label: "Remove", contents: [button])
+        }
+
+        private func makeLabeledStack(label labstr: String, contents conts: Array<MIInterfaceView>) -> MIStack {
+                let newbox = MIStack()
+                newbox.axis = .horizontal
+                let label = MILabel()
+                label.title = labstr
+                newbox.addArrangedSubView(label)
+                for cont in conts {
+                        newbox.addArrangedSubView(cont)
+                }
+                return newbox
+        }
+}
+
